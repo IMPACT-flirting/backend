7d0ac4df397615ead1d903d1a649ae0ebb22357f
anys34 <anyeseong34@gmail.com>
2024-01-27 22:38:09 +0900

    ADD :: postMapping

--- src/main/java/anys34/com/flirting/domain/place/presentation/PlaceController.java
+++ src/main/java/anys34/com/flirting/domain/place/presentation/PlaceController.java
@@ -3,6 +3,7 @@ package anys34.com.flirting.domain.place.presentation;
 import anys34.com.flirting.domain.place.presentation.dto.req.SavePlaceRequest;
 import anys34.com.flirting.domain.place.service.SavePlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlaceController {
     private final SavePlaceService savePlaceService;
 
+    @PostMapping("/save")
     public void save(@RequestBody SavePlaceRequest savePlaceRequest) {
         savePlaceService.execute(savePlaceRequest);
     }
